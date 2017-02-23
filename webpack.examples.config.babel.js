647bb8ebc2f4de9fcd274a18a6595307895c310b
Sarin Kizhakkepurayil <sarink@juniper.net>
2017-02-23 15:39:51 -0800

    updated package and webpack config

---
+++ webpack.examples.config.babel.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Juniper Networks, Inc. All rights reserved.
+ */
+import {join} from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin'
+
+const fileName = 'contrail-charts-examples'
+
+function absolute (...args) {
+  return join(__dirname, ...args)
+}
+const defaultEnv = {dev: true}
+
+export default (env = defaultEnv) => {
+  const plugins = [
+    new ExtractTextPlugin('css/' + fileName + '.css'),
+  ]
+  const loaders = [{
+    test: /\.scss$/,
+    loader: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: ['css-loader', 'sass-loader']
+    }),
+  }, {
+    test: /\.tmpl/,
+    loader: 'handlebars-loader',
+  }]
+
+  if (env.prod) {
+    plugins.push(new UglifyJSPlugin({
+      compress: {
+        warnings: false
+      },
+      mangle: {
+        keep_fnames: true,
+      },
+      sourceMap: true,
+      include: /\.js$/,
+    }))
+  }
+  loaders.push({
+    loader: 'babel-loader',
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    query: {
+      presets: ['es2015']
+    }
+  })
+
+  return {
+    entry: {
+      'example-loader': absolute('examples/common/js/example-loader')
+    },
+    devtool: 'source-map',
+    output: {
+      path: absolute('/build'),
+      filename: '[name].bundle.js'
+    },
+    module: {loaders},
+    externals: {
+      jquery: { amd: 'jquery', root: 'jQuery' },
+      d3: { amd: 'd3v4', root: 'd3' },
+      // lodash: { amd: 'lodash', root: '_' },
+      backbone: { amd: 'backbone', root: 'Backbone' },
+      coCharts: 'coCharts'
+    },
+    resolve: {
+      modules: [join(__dirname), 'node_modules'],
+      alias: {
+        'formatter': 'examples/common/js/value-formatters.js',
+        'constants': 'examples/common/js/constants.js',
+        'commons': 'examples/common/js/commons.js',
+        'data-generator': 'examples/common/js/data-generator.js'
+      },
+      extensions: ['.js']
+    },
+    plugins: plugins,
+    stats: { children: false }
+  }
+}
